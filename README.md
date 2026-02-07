f566fc9e4a31f22bb269e0fbaa12e150d7d91a7d
0xb10c <b10c@b10c.me>
2026-02-07 12:09:49 +0100

    add: tx-set support
    And update a bunch of stuff

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # tx-family-tree
 Tool to visualize parent and child (ancestors and descendants) relationships of transaction sets (blocks, mempool, ...)
+
+
+```
+$ rustup target add wasm32-unknown-unknown
+```
+
+```
+# Debug build (faster compilation, larger output)
+$ wasm-pack build --target web --dev
+
+# Release build (optimized for production)
+$ wasm-pack build --target web --release
+```
--- wasm-rawtx/src/lib.rs
+++ wasm-rawtx/src/lib.rs
@@ -34,27 +34,61 @@ pub fn graphdata_from_hexblock(hex: &str) -> JsValue {
     let block = deserialize_block(hex);
     let mut nodes = Vec::with_capacity(block.txdata.len());
     let mut links = Vec::new();
-    let known_txids: HashSet<bitcoin::Txid> = block.txdata.iter().map(|tx| tx.txid()).collect();
+    let known_txids: HashSet<bitcoin::Txid> =
+        block.txdata.iter().map(|tx| tx.compute_txid()).collect();
     for tx in block.txdata.iter() {
         if tx.is_coinbase() {
             continue;
         }
         let tx_info = TxInfo::new(&tx).unwrap();
         nodes.push(Node {
-            id: tx.txid().to_string(),
+            id: tx.compute_txid().to_string(),
             version: tx_info.version,
         });
         for (input, info) in tx.input.iter().zip(tx_info.input_infos) {
             if known_txids.contains(&input.previous_output.txid) {
                 links.push(Link {
-                    source: input.previous_output.txid.to_string(),
-                    target: tx.txid().to_string(),
+                    target: input.previous_output.txid.to_string(),
+                    source: tx.compute_txid().to_string(),
                     intype: info.in_type.to_string(),
                 });
             }
         }
     }
     serde_wasm_bindgen::to_value(&GraphData { nodes, links }).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn graphdata_from_hextxlist(hextxns: Vec<String>) -> JsValue {
+    let mut nodes = Vec::with_capacity(hextxns.len());
+    let txns: Vec<bitcoin::Transaction> = hextxns.iter().map(|hex| deserialize_tx(hex)).collect();
+    let mut links = Vec::new();
+    let known_txids: HashSet<bitcoin::Txid> = txns.iter().map(|tx| tx.compute_txid()).collect();
+    for tx in txns.iter() {
+        if tx.is_coinbase() {
+            continue;
+        }
+        let tx_info = TxInfo::new(&tx).unwrap();
+        nodes.push(Node {
+            id: tx.compute_txid().to_string(),
+            version: tx_info.version,
+        });
+        for (input, info) in tx.input.iter().zip(tx_info.input_infos) {
+            if known_txids.contains(&input.previous_output.txid) {
+                links.push(Link {
+                    target: input.previous_output.txid.to_string(),
+                    source: tx.compute_txid().to_string(),
+                    intype: info.in_type.to_string(),
+                });
+            }
+        }
+    }
+    serde_wasm_bindgen::to_value(&GraphData { nodes, links }).unwrap()
+}
+
+fn deserialize_tx(hex: &str) -> bitcoin::Transaction {
+    let tx_bytes = Vec::from_hex(hex).unwrap();
+    bitcoin::consensus::deserialize(&tx_bytes).unwrap()
 }
 
 fn deserialize_block(hex: &str) -> bitcoin::Block {
